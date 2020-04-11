ea13172ec38d3664ae19135c002f4dc9f7b2fb87
VictoriaColone <1449515567@qq.com>
2020-04-11 13:56:23 +0800

    Vshow 侧滑栏+本地视频流+沉浸式

---
+++ app/src/main/java/com/victoria/vshow/Activity/MainActivity.java
@@ -0,0 +1,216 @@
+package com.victoria.vshow.Activity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.victoria.vshow.R;
+import com.victoria.vshow.viewpager.OnViewPagerListener;
+import com.victoria.vshow.viewpager.ViewPagerLayoutManager;
+
+import androidx.annotation.ColorInt;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.OrientationHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * @author Victoria Colone
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private RecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
+    private ViewPagerLayoutManager mLayoutManager;
+
+    // 图片，视频流资源
+    private int[] imgs = {R.mipmap.img_video_1, R.mipmap.img_video_2};
+    private int[] videos = {R.raw.video_1, R.raw.video_2};
+
+    private RadioGroup rg_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initListener();
+    }
+
+    //沉浸式处理
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    private void initView() {
+
+        rg_main = findViewById(R.id.rg_main);
+        rg_main.check(R.id.main_for_show);
+        mRecyclerView = findViewById(R.id.recycler);
+        mLayoutManager = new ViewPagerLayoutManager(this, OrientationHelper.VERTICAL);
+        mAdapter = new MyAdapter();
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void initListener() {
+
+        //响应视频上的操作
+        mLayoutManager.setOnViewPagerListener(new OnViewPagerListener() {
+
+            @Override
+            public void onInitComplete() {
+                Log.e(TAG, "onInitComplete");
+                playVideo(0);
+            }
+
+            @Override
+            public void onPageRelease(boolean isNext, int position) {
+                Log.e(TAG, "释放位置:" + position + " 下一页:" + isNext);
+                int index = 0;
+                if (isNext) {
+                    index = 0;
+                } else {
+                    index = 1;
+                }
+                releaseVideo(index);
+            }
+
+            @Override
+            public void onPageSelected(int position, boolean isBottom) {
+                Log.e(TAG, "选中位置:" + position + "  是否是滑动到底部:" + isBottom);
+                playVideo(0);
+            }
+        });
+
+
+
+    }
+
+    //播放视频
+    private void playVideo(int position) {
+        View itemView = mRecyclerView.getChildAt(0);
+        final VideoView videoView = itemView.findViewById(R.id.video_view);
+        final ImageView imgPlay = itemView.findViewById(R.id.img_play);
+        final ImageView imgThumb = itemView.findViewById(R.id.img_thumb);
+        final RelativeLayout rootView = itemView.findViewById(R.id.root_view);
+        final MediaPlayer[] mediaPlayer = new MediaPlayer[1];
+        videoView.start();
+
+        videoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                mediaPlayer[0] = mp;
+                mp.setLooping(true);
+                imgThumb.animate().alpha(0).setDuration(200).start();
+                return false;
+            }
+        });
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+            }
+        });
+
+
+        imgPlay.setOnClickListener(new View.OnClickListener() {
+            boolean isPlaying = true;
+
+            @Override
+            public void onClick(View v) {
+                if (videoView.isPlaying()) {
+                    imgPlay.animate().alpha(1f).start();
+                    videoView.pause();
+                    isPlaying = false;
+                } else {
+                    imgPlay.animate().alpha(0f).start();
+                    videoView.start();
+                    isPlaying = true;
+                }
+            }
+        });
+    }
+
+    //
+    private void releaseVideo(int index) {
+        View itemView = mRecyclerView.getChildAt(index);
+        final VideoView videoView = itemView.findViewById(R.id.video_view);
+        final ImageView imgThumb = itemView.findViewById(R.id.img_thumb);
+        final ImageView imgPlay = itemView.findViewById(R.id.img_play);
+        videoView.stopPlayback();
+        imgThumb.animate().alpha(1).start();
+        imgPlay.animate().alpha(0f).start();
+    }
+
+
+   class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+
+        public MyAdapter() {
+        }
+
+
+        //布局填充
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view_pager, parent, false);
+            return new ViewHolder(view);
+        }
+
+        //解析图片与视频资源
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.img_thumb.setImageResource(imgs[position % 2]);
+            holder.videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + videos[position % 2]));
+        }
+
+        //项目总数
+        @Override
+        public int getItemCount() {
+            return 20;
+        }
+
+        //视频对象类
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView img_thumb;
+            VideoView videoView;
+            ImageView img_play;
+            RelativeLayout rootView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                img_thumb = itemView.findViewById(R.id.img_thumb);
+                videoView = itemView.findViewById(R.id.video_view);
+                img_play = itemView.findViewById(R.id.img_play);
+                rootView = itemView.findViewById(R.id.root_view);
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/victoria/vshow/Activity/SplashActivity.java
@@ -0,0 +1,68 @@
+package com.victoria.vshow.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.victoria.vshow.R;
+
+
+/**
+ * @author Victoria Colone
+ */
+public class SplashActivity extends Activity {
+
+    private static final String TAG = "SplashActivity";
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //无title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //全屏
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        //异步消息机制延时发送跳转页面信息
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //在主线程执行
+                startMainActivity();
+            }
+        },2000);
+    }
+
+    //设置点击后快速进去,为防止多次启动，要在manifest文件中设置为单例
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        startMainActivity();
+
+        return super.onTouchEvent(event);
+    }
+
+    //跳转主界面
+    private void startMainActivity() {
+        Log.d(TAG, " startMainActivity ");
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        //关闭启动页面
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        //在活动销毁时移除handler消息，否则在进入appM(SplashActivity)立刻点击返回，还是会唤醒ainActivity
+        handler.removeCallbacksAndMessages(null);
+    }
+}
