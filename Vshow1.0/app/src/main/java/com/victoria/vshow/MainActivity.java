e2d50da789ff99e0248f7ab159a4db82e724c39c
VictoriaColone <1449515567@qq.com>
2020-04-21 21:37:41 +0800

    仓库整理，分出vshow0.1，vhsow1.0和学习项目ppjoke

---
+++ Vshow1.0/app/src/main/java/com/victoria/vshow/MainActivity.java
@@ -0,0 +1,38 @@
+package com.victoria.vshow;
+
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * @author Victoria Colone
+ */
+public class MainActivity extends AppCompatActivity {
+
+    //沉浸式处理
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
