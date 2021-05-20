576b72da37632ad3779cc66514215f9202bde1ab
ximao.yt <ximao.yt@alibaba-inc.com>
2021-05-20 19:54:49 +0800

    checkstyle test

--- Vshow0.1/app/src/main/java/com/victoria/vshow/base/BaseFragment.java
+++ Vshow0.1/app/src/main/java/com/victoria/vshow/base/BaseFragment.java
@@ -4,5 +4,5 @@ package com.victoria.vshow.base;
  * fragment基类
  */
 public class BaseFragment {
-    private static final String adf = "1231231231231ddddddddddddddd23123123123123123123123124343412342453234521324353214211233421423132";
+    private static final String adfasdfffffffffffffff = "1231231231231ddddddddddddddd23123123123123123123123124343412342453234521324353dfasdfad214211233421423132";
 }
