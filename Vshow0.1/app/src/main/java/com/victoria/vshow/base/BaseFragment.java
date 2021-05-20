3534a3aae2f73edb80fddb966e10e1eae0db8497
ximao.yt <ximao.yt@alibaba-inc.com>
2021-05-20 19:53:25 +0800

    checkstyle test

--- Vshow0.1/app/src/main/java/com/victoria/vshow/base/BaseFragment.java
+++ Vshow0.1/app/src/main/java/com/victoria/vshow/base/BaseFragment.java
@@ -4,4 +4,5 @@ package com.victoria.vshow.base;
  * fragment基类
  */
 public class BaseFragment {
+    private static final String adf = "123123123123123123123123123123123123124343412342453234521324353214211233421423132";
 }
